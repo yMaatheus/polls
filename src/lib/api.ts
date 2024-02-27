ea7b3b201d65c77b6f87256049516272e4fb0783
Matheus <ymatheus.h@gmail.com>
2024-02-27 01:02:05 -0300

    feat: :sparkles: Add .env file and API module

---
+++ src/lib/api.ts
@@ -0,0 +1,20 @@
+import { env } from "./env";
+
+export async function api<T>(path: string, init?: RequestInit) {
+  const baseUrl = env.NEXT_PUBLIC_API_BASE_URL;
+  const url = new URL(path, baseUrl);
+
+  const response = await fetch(url, { credentials: "include", ...init });
+
+  if (!response.ok) {
+    console.error(response.statusText);
+    return;
+  }
+
+  const data = await response.json() as T;
+
+  return {
+    ...response,
+    data,
+  };
+}
---
+++ src/lib/env.ts
@@ -0,0 +1,16 @@
+import { createEnv } from '@t3-oss/env-nextjs'
+import { z } from 'zod'
+
+export const env = createEnv({
+  server: {},
+
+  client: {
+    NEXT_PUBLIC_API_BASE_URL: z.string().url(),
+    NEXT_PUBLIC_WEB_SOCKET_BASE_URL: z.string().url(),
+  },
+
+  runtimeEnv: {
+    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
+    NEXT_PUBLIC_WEB_SOCKET_BASE_URL: process.env.NEXT_PUBLIC_WEB_SOCKET_BASE_URL,
+  },
+})
