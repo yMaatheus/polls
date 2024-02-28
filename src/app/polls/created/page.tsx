9cb0dcfe061d311f099d5951cff6c889d7f89c1c
Matheus <ymatheus.h@gmail.com>
2024-02-28 16:44:33 -0300

    feat: :sparkles: Add copy to clipboard functionality to poll creation page

--- src/app/polls/created/page.tsx
+++ src/app/polls/created/page.tsx
@@ -1,7 +1,18 @@
 'use client'
 
-import Link from 'next/link'
+import { Button } from '@/components/ui/button'
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@/components/ui/tooltip'
+import { toast } from '@/components/ui/use-toast'
+import { env } from '@/lib/env'
+import { ClipboardIcon } from '@radix-ui/react-icons'
 import { useSearchParams } from 'next/navigation'
+
+const BASE_URL = env.NEXT_PUBLIC_BASE_URL
 
 export default function Page() {
   const searchParams = useSearchParams()
@@ -10,21 +21,46 @@ export default function Page() {
 
   if (!pollId) return null
 
+  async function handleCopyToClipboard() {
+    try {
+      const URL = `${BASE_URL}/polls/${pollId}`
+      await navigator.clipboard.writeText(URL)
+
+      toast({
+        title: 'Clipboard',
+        description: 'Link copiado com sucesso!',
+      })
+    } catch (error) {
+      console.error('Failed to copy link to clipboard: ', error)
+    }
+  }
+
   return (
-    <div className="flex flex-1 items-center justify-center h-full">
-      <div className="w-96 min-h-96 bg-neutral-900 rounded-lg flex flex-col items-center justify-around p-8">
-        <h1 className="font-semibold text-2xl">Parabéns!</h1>
-        <h1 className="font-semibold text-2xl">Você criou uma enquete!</h1>
-        <p className="text-center">
-          Compartilhe o link para que seus amigos possam votar.
-        </p>
-        <Link
-          href={`/polls/${pollId}`}
-          className="text-primary-500 hover:underline"
-        >
-          {`/polls/${pollId}`}
-        </Link>
-      </div>
-    </div>
+    <TooltipProvider>
+      <Tooltip>
+        <div className="flex flex-1 items-center justify-center h-full">
+          <div className="min-w-96 min-h-64 bg-neutral-900 rounded-lg flex flex-col justify-center items-center space-y-6 p-8">
+            <h1 className="font-semibold text-xl items-start">
+              Parabéns, Enquete criada com sucesso!
+            </h1>
+            <div className="flex flex-col items-center justify-center space-y-4">
+              <p className="text-center">
+                Compartilhe o link para que seus amigos possam votar.
+              </p>
+              <TooltipTrigger asChild>
+                <Button variant="ghost" onClick={handleCopyToClipboard}>
+                  Copiar Link
+                  <ClipboardIcon className="ml-2 h-4 w-4" />
+                </Button>
+              </TooltipTrigger>
+            </div>
+          </div>
+        </div>
+
+        <TooltipContent>
+          <p>Click to copy</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   )
 }
