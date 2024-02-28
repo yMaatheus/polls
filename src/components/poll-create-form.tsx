dc157df1f57ba11334e910075c5331181c6475f3
Matheus <ymatheus.h@gmail.com>
2024-02-28 18:48:03 -0300

    feat: :sparkles: Add error handling to poll creation

--- src/components/poll-create-form.tsx
+++ src/components/poll-create-form.tsx
@@ -7,6 +7,7 @@ import { FormEvent, useState } from 'react'
 import { Button } from './ui/button'
 import { Input } from './ui/input'
 import { Label } from './ui/label'
+import { toast } from './ui/use-toast'
 
 export function PollCreateForm() {
   const router = useRouter()
@@ -31,22 +32,36 @@ export function PollCreateForm() {
     const { title } = Object.fromEntries(formData) as { title: string }
     const optionsNoEmpty = options.filter((option) => option !== '')
 
-    const response = await api<CreatePollResponse>('/polls', {
-      method: 'POST',
-      headers: {
-        'Content-type': 'application/json',
-      },
-      body: JSON.stringify({
-        title,
-        options: optionsNoEmpty,
-      }),
-    })
+    try {
+      const response = await api<CreatePollResponse>('/polls', {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify({
+          title,
+          options: optionsNoEmpty,
+        }),
+      })
 
-    if (!response || !response.data) return
+      const { data } = response
 
-    const { data } = response
-
-    router.push(`/polls/created?pollId=${data.pollId}`)
+      router.push(`/polls/created?pollId=${data.pollId}`)
+    } catch (error) {
+      if (!(error instanceof Error)) {
+        toast({
+          title: 'Enquete',
+          description: 'Falha ao tentar criar uma enquete.',
+          variant: 'destructive',
+        })
+        return
+      }
+      toast({
+        title: 'Enquete',
+        description: error.message,
+        variant: 'destructive',
+      })
+    }
   }
 
   return (
