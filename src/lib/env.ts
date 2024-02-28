27276782c739d1d5775739eb97a91154356329a5
Matheus <ymatheus.h@gmail.com>
2024-02-28 16:43:45 -0300

    feat: :wrench: Add NEXT_PUBLIC_BASE_URL to .env and env.ts

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -5,11 +5,13 @@ export const env = createEnv({
   server: {},
 
   client: {
+    NEXT_PUBLIC_BASE_URL: z.string().url(),
     NEXT_PUBLIC_API_BASE_URL: z.string().url(),
     NEXT_PUBLIC_WEB_SOCKET_BASE_URL: z.string().url(),
   },
 
   runtimeEnv: {
+    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
     NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
     NEXT_PUBLIC_WEB_SOCKET_BASE_URL:
       process.env.NEXT_PUBLIC_WEB_SOCKET_BASE_URL,
